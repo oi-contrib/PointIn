47931ac47608052e0ecb197700a124e2c922bb0d
zxl20070701 <1904314465@qq.com>
2026-01-18 21:30:25 +0800

    修复rect不支持负数问题

--- src/rect.js
+++ src/rect.js
@@ -1,3 +1,14 @@
 export default function (x, y, width, height) {
+
+    if (width < 0) {
+        x += width;
+        width *= -1;
+    }
+
+    if (height < 0) {
+        y += height;
+        height *= -1;
+    }
+
     return this.x >= x && this.x <= x + width && this.y >= y && this.y <= y + height;
 };
